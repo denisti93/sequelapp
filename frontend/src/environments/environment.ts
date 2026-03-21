c142bf740531e299203a7662b9625fe1474bc9b6
Denis Neres Caminha <caminhadenis@Deniss-MacBook-Air.local>
2026-03-21 16:41:33 -0300

    modifica url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://sequelapp.onrender.com',
   webPushPublicKey: 'BMVtNZpuXEZe_RmHi4Ep2vY5TfD5aD9reAkWP6afi3B5fZ1z5O1T5JQcCsJDPRWiHyLYkuHxUkUuAw6q0DzBhbs'
 };
